5d158be433e00e1fb6d9f1a0f14de0c59b1c666b
tseyu <newbeeu2022@outlook.com>
2024-02-05 20:43:13 +0800

    add Ch1-2

---
+++ Ch1/Ch1-2/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!\n"; //std::cout lives in the iostream library
+    std::cout << " \n"; // These comments make the code hard to read
+    std::cout << " \n"; // especially when lines are different when lines are different lengths
+
+    // It's a better way.
+    std::cout << " \n";
+
+    /*
+     * This is a multi-line comment.
+     * This layout makes the comment easier to read.
+     */
+
+    /*
+     * GOOD COMMENTS AND BAD COMMENTS
+     */
+    std::cout << " \n";
+    return 0;
+}
