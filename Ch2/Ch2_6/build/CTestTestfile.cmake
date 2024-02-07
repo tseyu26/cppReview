2c57cf3bb7a6eabfb2d27f507ea237d6746d0359
tseyu <newbeeu2022@outlook.com>
2024-02-07 18:08:21 +0800

    add Ch2_6

---
+++ Ch2/Ch2_6/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tseyu/cppReview/Ch2/Ch2_6
+# Build directory: /home/tseyu/cppReview/Ch2/Ch2_6/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ Ch2/Ch2_6/main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main(int, char**){
+    std::cout << "Hello, from Ch2_6!\n";
+}
