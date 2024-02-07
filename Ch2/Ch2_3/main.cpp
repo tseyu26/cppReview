fb9c6f7a11b5755fde3ae4e02e39460d480cc081
tseyu <newbeeu2022@outlook.com>
2024-02-07 17:28:33 +0800

    add Ch2_3

---
+++ Ch2/Ch2_3/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+// void means the function does not return a value to the caller
+void printHi()
+{
+    std::cout << "Hi" << '\n';
+
+    // This function does not return a value so no return statement is needed
+}
+
+int main()
+{
+    printHi(); // okay: function printHi() is called, no value is returned
+
+    return 0;
+}
