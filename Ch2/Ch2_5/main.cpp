08bf1fac057cb374fafb66f5931b502dd23ec910
tseyu <newbeeu2022@outlook.com>
2024-02-07 18:02:19 +0800

    add Ch2_5

---
+++ Ch2/Ch2_5/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+// x is not in scope anywhere in this function
+void doSomething()
+{
+    std::cout << "Hello!\n";
+}
+
+int main()
+{
+    // x can not be used here because it's not in scope yet
+
+    int x{ 0 }; // x enters scope here and can now be used within this function
+
+    doSomething();
+
+    return 0;
+} // x goes out of scope here and can no longer be used
