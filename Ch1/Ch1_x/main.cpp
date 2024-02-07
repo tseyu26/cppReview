d1aaeb7ddd7713b914e910050794d28c703f8160
tseyu <newbeeu2022@outlook.com>
2024-02-07 12:30:15 +0800

    add Ch1_x

---
+++ Ch1/Ch1_x/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+/* A statement is a type of instruction that 
+ * causes the program to perform some action. 
+ * Statements are often terminated by a semicolon.
+ *
+ * A function is a collection of statements 
+ * that execute sequentially. 
+ * Every C++ program must include a special function named main.
+ * When you run your program, execution starts 
+ * at the top of the main function.
+ * 
+ * In programming, the name of a function 
+ * (or object, type, template, etc…) is called its identifier.
+ * 
+ * 
+ */
+
+
+int main(int, char**){
+    std::cout << "Hello, from Ch1_x!\n";
+}
