bfc581cea29cece65bbc427991bae68b4181cf30
tseyu <newbeeu2022@outlook.com>
2024-02-07 11:57:49 +0800

    add Ch1_7

---
+++ Ch1/Ch1_7/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    std::cout << "Hello, from Ch1_7!\n";
+    int value; // conventional
+
+    int Value; // unconventional (should start with lower case letter)
+    int VALUE; // unconventional (should start with lower case letter and be in all lower case)
+    int VaLuE; // unconventional (see your psychiatrist) ;)
+    return 0;
+}
