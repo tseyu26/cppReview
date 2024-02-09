e99d9f471f9f8c2fe25e0f77e2230a3161186924
tseyu <newbeeu2022@outlook.com>
2024-02-09 11:49:48 +0800

    add Ch4_5

---
+++ Ch4/Ch4_5/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+    unsigned short x{ 65535 }; // largest 16-bit unsigned value possible
+    std::cout << "x was: " << x << '\n';
+
+    x = 65536; // 65536 is out of our range, so we get modulo wrap-around
+    std::cout << "x is now: " << x << '\n';
+
+    x = 65537; // 65537 is out of our range, so we get modulo wrap-around
+    std::cout << "x is now: " << x << '\n';
+
+    x = -1; // -1 is out of our range, so we get modulo wrap-around
+    std::cout << "x is now: " << x << '\n';
+
+    x = -2; // -2 is out of our range, so we get modulo wrap-around
+    std::cout << "x is now: " << x << '\n';
+
+    return 0;
+}
