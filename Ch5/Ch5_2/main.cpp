2416c1a7cf7151eacecf08438ec7b3322954c682
tseyu <newbeeu2022@outlook.com>
2024-02-13 13:36:42 +0800

    add Ch5_2

---
+++ Ch5/Ch5_2/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << 5.0 << '\n';  // 5.0 (no suffix) is type double (by default)
+    std::cout << 5.0f << '\n'; // 5.0f is type float
+
+    return 0;
+}
