06c4088df4eda53f7b257339612df96562cabf7e
tseyu <newbeeu2022@outlook.com>
2024-02-14 21:40:13 +0800

    add Ch5_9

---
+++ Ch5/Ch5_9/build/CMakeFiles/Ch5_9.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ch5_9.dir/main.cpp.o"
+  "CMakeFiles/Ch5_9.dir/main.cpp.o.d"
+  "Ch5_9"
+  "Ch5_9.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ch5_9.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Ch5/Ch5_9/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "Pick 1 or 2: ";
+    int choice{};
+    std::cin >> choice;
+
+    std::cout << "Now enter your name: ";
+    std::string name{};
+    std::getline(std::cin >> std::ws, name); // note: added std::ws here
+
+    std::cout << "Hello, " << name << ", you picked " << choice << '\n';
+
+    return 0;
+}
