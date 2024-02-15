df0dc77c451a28377f0371c8ef887316c7b7f692
tseyu <newbeeu2022@outlook.com>
2024-02-15 12:05:50 +0800

    add Ch6_1

---
+++ Ch6/Ch6_1/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int getValue()
+{
+    std::cout << "Enter an integer: ";
+
+    int x{};
+    std::cin >> x;
+    return x;
+}
+
+void printCalculation(int x, int y, int z)
+{
+    std::cout << x + (y * z) << "\n";
+}
+int main()
+{
+    int a{ getValue() }; // will execute first
+    int b{ getValue() }; // will execute second
+    int c{ getValue() }; // will execute third
+
+    printCalculation(a, b, c); // this line is now unambiguous
+
+    return 0;
+}
