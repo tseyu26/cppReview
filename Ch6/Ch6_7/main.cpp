f894da4c516d6ee0a3b1c159a260ed98644af303
tseyu <newbeeu2022@outlook.com>
2024-02-18 14:41:53 +0800

    add Ch6_7

---
+++ Ch6/Ch6_7/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    int x{ 5 };
+    int y{ 7 };
+
+    if (!(x > y))
+        std::cout << x << " is not greater than " << y << '\n';
+    else
+        std::cout << x << " is greater than " << y << '\n';
+
+    return 0;
+}
