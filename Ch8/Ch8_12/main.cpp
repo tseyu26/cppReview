7690960b0e140f19f6cece735185b8de6d393961
tseyu <newbeeu2022@outlook.com>
2024-02-19 18:35:20 +0800

    add Ch8_12

---
+++ Ch8/Ch8_12/main.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib> // for std::abort()
+#include <iostream>
+
+int main()
+{
+    std::cout << 1 << '\n';
+    std::abort();
+
+    // The following statements never execute
+    std::cout << 2 << '\n';
+
+    return 0;
+}
