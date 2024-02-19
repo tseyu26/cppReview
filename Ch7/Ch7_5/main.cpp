e91f63d6c9b5f858cb285fb1b035eec6e44b5b81
tseyu <newbeeu2022@outlook.com>
2024-02-19 11:39:01 +0800

    add Ch7_5

---
+++ Ch7/Ch7_5/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main()
+{ // outer block
+    int apples { 5 }; // here's the outer block apples
+
+    { // nested block
+        // apples refers to outer block apples here
+        std::cout << apples << '\n'; // print value of outer block apples
+
+        int apples{ 0 }; // define apples in the scope of the nested block
+        // int apples { 0 } there will be a different result without defining the 'apples'
+        // apples now refers to the nested block apples
+        // the outer block apples is temporarily hidden
+
+        apples = 10; // this assigns value 10 to nested block apples, not outer block apples
+
+        std::cout << apples << '\n'; // print value of nested block apples
+    } // nested block apples destroyed
+
+
+    std::cout << apples << '\n'; // prints value of outer block apples
+
+    return 0;
+} // outer block apples destroyed
