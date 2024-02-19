d30f664d12bb1a7e9adaf2bc56026791c8f613a3
tseyu <newbeeu2022@outlook.com>
2024-02-19 15:53:39 +0800

    add Ch8_2

---
+++ Ch8/Ch8_2/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tseyu/cppReview/Ch8/Ch8_2
+# Build directory: /home/tseyu/cppReview/Ch8/Ch8_2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
