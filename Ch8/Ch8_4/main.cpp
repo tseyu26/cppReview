3b049b105c104646ee2157635e507f44caf929c5
tseyu <newbeeu2022@outlook.com>
2024-02-19 16:15:03 +0800

    add Ch8_4

---
+++ Ch8/Ch8_4/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+	constexpr double gravity{ 9.8 };
+
+	if constexpr (gravity == 9.8) // now using constexpr if
+		std::cout << "Gravity is normal.\n";
+	else
+		std::cout << "We are not on Earth.\n";
+
+	return 0;
+}
