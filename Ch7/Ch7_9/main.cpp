8d7950cd6b45c5103b96edaf24407ed710c72578
tseyu <newbeeu2022@outlook.com>
2024-02-19 13:50:05 +0800

    add Ch7_9

---
+++ Ch7/Ch7_9/main.cpp
@@ -0,0 +1,14 @@
+#include "constants.h"
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter a radius: ";
+    double radius{};
+    std::cin >> radius;
+
+    std::cout << "The circumference is: " << 2 * radius * constants::pi << '\n';
+
+    return 0;
+}
