014f9febad95a27f3ab56a291587d2a59624fa4c
tseyu <newbeeu2022@outlook.com>
2024-02-20 17:55:35 +0800

    add Ch12_8

---
+++ Ch12/Ch12_8/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    int x { 5 };
+    int* ptr { &x };
+
+    // pointers convert to Boolean false if they are null, and Boolean true if they are non-null
+    if (ptr) // implicit conversion to Boolean
+        std::cout << "ptr is non-null\n";
+    else
+        std::cout << "ptr is null\n";
+
+    int* nullPtr {};
+    std::cout << "nullPtr is " << (nullPtr ? "non-null\n" : "null\n"); // implicit conversion to Boolean
+
+    return 0;
+}
