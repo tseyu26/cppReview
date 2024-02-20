90158df749a03683309449f787536f2325f6382c
tseyu <newbeeu2022@outlook.com>
2024-02-20 14:38:46 +0800

    add Ch11_4

---
+++ Ch11/Ch11_4/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+void printInt(int x)
+{
+    std::cout << x << '\n';
+}
+
+void printInt(char) = delete; // calls to this function will halt compilation
+void printInt(bool) = delete; // calls to this function will halt compilation
+
+int main()
+{
+    printInt(97);   // okay
+
+    printInt('a');  // compile error: function deleted
+    printInt(true); // compile error: function deleted
+
+    printInt(5.0);  // compile error: ambiguous match
+
+    return 0;
+}
