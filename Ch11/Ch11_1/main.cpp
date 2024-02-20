2add9cdacc1163cfbd3c057ca41931a63149160b
tseyu <newbeeu2022@outlook.com>
2024-02-20 14:26:34 +0800

    add Ch11_1

---
+++ Ch11/Ch11_1/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int add(int x, int y)
+{
+    return x + y;
+}
+
+double add(double x, double y)
+{
+    return x + y;
+}
+
+int main()
+{
+    std::cout << add(1, 2); // calls add(int, int)
+    std::cout << '\n';
+    std::cout << add(1.2, 3.4); // calls add(double, double)
+    std::cout << '\n';
+    
+    return 0;
+}
