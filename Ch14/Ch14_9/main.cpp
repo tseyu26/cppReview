0cee40205da707ee6a2d7e8f85ad372d032ebba2
tseyu <newbeeu2022@outlook.com>
2024-02-21 18:06:43 +0800

    add Ch14_9

---
+++ Ch14/Ch14_9/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+class Foo
+{
+private:
+    int m_x {};
+    int m_y {};
+
+public:
+    Foo(int x, int y) // here's our constructor function that takes two initializers
+    {
+        std::cout << "Foo(" << x << ", " << y << ") constructed\n";
+    }
+
+    void print() const
+    {
+        std::cout << "Foo(" << m_x << ", " << m_y << ")\n";
+    }
+};
+
+int main()
+{
+    Foo foo{ 6, 7 }; // calls Foo(int, int) constructor
+    foo.print();
+
+    return 0;
+}
