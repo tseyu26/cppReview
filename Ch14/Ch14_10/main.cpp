02a943f32ed58575e3d89676ecb26387a0146b5b
tseyu <newbeeu2022@outlook.com>
2024-02-21 18:24:41 +0800

    add Ch14_10

---
+++ Ch14/Ch14_10/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+class Foo
+{
+private:
+    int m_x {};
+    int m_y {};
+
+public:
+    Foo(int x, int y)
+        : m_x { x }, m_y { y } // here's our member initialization list
+    {
+        std::cout << "Foo(" << x << ", " << y << ") constructed\n";
+    }
+
+    void print() const
+    {
+        std::cout << "Foo(" << m_x << ", " << m_y << ")\n";
+    }
+};
+
+int main()
+{
+    Foo foo{ 6, 7 };
+    foo.print();
+
+    return 0;
+}
