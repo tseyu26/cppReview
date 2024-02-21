acfc30a70d46f8354c8337bab0aa39b0a9034c28
tseyu <newbeeu2022@outlook.com>
2024-02-21 14:37:36 +0800

    add Ch13_3

---
+++ Ch13/Ch13_3/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <utility> // for std::to_underlying() (C++23)
+
+int main()
+{
+    enum class Color
+    {
+        red,
+        blue,
+    };
+
+    Color color { Color::blue };
+
+    //std::cout << color << '\n'; // won't work, because there's no implicit conversion to int
+    std::cout << static_cast<int>(color) << '\n';   // explicit conversion to int, will print 1
+    std::cout << std::to_underlying(color) << '\n'; // convert to underlying type, will print 1 (C++23)
+
+    return 0;
+}
