2c8ec4bc47a9485c100d70e8618bf7058685ca0f
tseyu <newbeeu2022@outlook.com>
2024-02-22 15:42:15 +0800

    add Ch16_4

---
+++ Ch16/Ch16_4/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+void passByRef(const std::vector<T>& arr)
+{
+    std::cout << arr[0] << '\n';
+}
+
+int main()
+{
+    std::vector primes{ 2, 3, 5, 7, 11 };
+    passByRef(primes); // ok: compiler will instantiate passByRef(const std::vector<int>&)
+
+    std::vector dbl{ 1.1, 2.2, 3.3 };
+    passByRef(dbl);    // ok: compiler will instantiate passByRef(const std::vector<double>&)
+
+    return 0;
+}
