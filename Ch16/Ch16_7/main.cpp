dfe7b699b9b7743684b3907840fc25fa55fa0cf4
tseyu <newbeeu2022@outlook.com>
2024-02-22 16:01:06 +0800

    add Ch16_7

---
+++ Ch16/Ch16_7/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+void printReverse(const std::vector<T>& arr)
+{
+    for (int index{ static_cast<int>(arr.size()) - 1}; index >= 0; --index) // index is signed
+    {
+        std::cout << arr[static_cast<std::size_t>(index)] << ' ';
+    }
+
+    std::cout << '\n';
+}
+
+int main()
+{
+    std::vector arr{ 4, 6, 7, 3, 8, 2, 1, 9 };
+
+    printReverse(arr);
+
+    return 0;
+}
