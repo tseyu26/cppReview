178cf4889cace7f89c1510a71ac07fcd8d31d936
tseyu <newbeeu2022@outlook.com>
2024-02-22 15:13:34 +0800

    add Ch16_2

---
+++ Ch16/Ch16_2/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector primes { 2, 3, 5, 7, 11 }; // hold the first 5 prime numbers (as int)
+
+    std::cout << "An int is " << sizeof(int) << " bytes\n";
+    std::cout << &(primes[0]) << '\n';
+    std::cout << &(primes[1]) << '\n';
+    std::cout << &(primes[2]) << '\n';
+
+    return 0;
+}
