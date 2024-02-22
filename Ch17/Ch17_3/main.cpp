f86484c67287572b852e24ae71a88dfd0ce2ae8a
tseyu <newbeeu2022@outlook.com>
2024-02-22 18:02:43 +0800

    add Ch17_3

---
+++ Ch17/Ch17_3/main.cpp
@@ -0,0 +1,22 @@
+#include <array>
+#include <iostream>
+
+template <typename T, auto N> // note that this template parameter declaration matches the one for std::array
+void passByRef(const std::array<T, N>& arr)
+{
+    std::cout << arr[0] << '\n';
+}
+
+int main()
+{
+    std::array arr{ 9, 7, 5, 3, 1 }; // use CTAD to infer std::array<int, 5>
+    passByRef(arr);  // ok: compiler will instantiate passByRef(const std::array<int, 5>& arr)
+
+    std::array arr2{ 1, 2, 3, 4, 5, 6 }; // use CTAD to infer std::array<int, 6>
+    passByRef(arr2); // ok: compiler will instantiate passByRef(const std::array<int, 6>& arr)
+
+    std::array arr3{ 1.2, 3.4, 5.6, 7.8, 9.9 }; // use CTAD to infer std::array<double, 5>
+    passByRef(arr3); // ok: compiler will instantiate passByRef(const std::array<double, 5>& arr)
+
+    return 0;
+}
