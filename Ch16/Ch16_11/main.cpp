0aa4f1a1c871eddbe4b35564ae9228cc68d6c2f4
tseyu <newbeeu2022@outlook.com>
2024-02-22 17:00:57 +0800

    add Ch16_11

---
+++ Ch16/Ch16_11/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+void printStack(const std::vector<int>& stack)
+{
+	if (stack.empty()) // if stack.size == 0
+		std::cout << "Empty";
+
+	for (auto element : stack)
+		std::cout << element << ' ';
+
+	// \t is a tab character, to help align the text
+	std::cout << "\tCapacity: " << stack.capacity() << "  Length " << stack.size() << "\n";
+}
+
+int main()
+{
+	std::vector<int> stack{}; // empty stack
+
+	printStack(stack);
+
+	stack.push_back(1); // push_back() pushes an element on the stack
+	printStack(stack);
+
+	stack.push_back(2);
+	printStack(stack);
+
+	stack.push_back(3);
+	printStack(stack);
+
+	std::cout << "Top: " << stack.back() << '\n'; // back() returns the last element
+
+	stack.pop_back(); // pop_back() pops an element off the stack
+	printStack(stack);
+
+	stack.pop_back();
+	printStack(stack);
+
+	stack.pop_back();
+	printStack(stack);
+
+	return 0;
+}
