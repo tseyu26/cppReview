34bf93e04b5acb049e623520daf3dd1d6683bead
tseyu <newbeeu2022@outlook.com>
2024-02-22 13:49:41 +0800

    add Ch15_4

---
+++ Ch15/Ch15_4/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+class Simple
+{
+private:
+    int m_id {};
+
+public:
+    Simple(int id)
+        : m_id { id }
+    {
+        std::cout << "Constructing Simple " << m_id << '\n';
+    }
+
+    ~Simple() // here's our destructor
+    {
+        std::cout << "Destructing Simple " << m_id << '\n';
+    }
+
+    int getID() const { return m_id; }
+};
+
+int main()
+{
+    // Allocate a Simple
+    Simple simple1{ 1 };
+    {
+        Simple simple2{ 2 };
+    } // simple2 dies here
+
+    return 0;
+} // simple1 dies here
