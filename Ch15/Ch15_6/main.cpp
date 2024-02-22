adad2d9e8d57b4245a7da6aeade48ff1a3e3aae2
tseyu <newbeeu2022@outlook.com>
2024-02-22 14:24:28 +0800

    add Ch15_6

---
+++ Ch15/Ch15_6/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+class Something
+{
+private:
+    static inline int s_idGenerator { 1 };
+    int m_id {};
+
+public:
+    // grab the next value from the id generator
+    Something() : m_id { s_idGenerator++ }
+    {
+    }
+
+    int getID() const { return m_id; }
+};
+
+int main()
+{
+    Something first{};
+    Something second{};
+    Something third{};
+
+    std::cout << first.getID() << '\n';
+    std::cout << second.getID() << '\n';
+    std::cout << third.getID() << '\n';
+    return 0;
+}
