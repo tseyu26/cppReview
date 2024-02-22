369a01586804bd794546cb9cbdc65e5a736e99bd
tseyu <newbeeu2022@outlook.com>
2024-02-22 18:33:32 +0800

    add Ch17_7

---
+++ Ch17/Ch17_7/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tseyu/cppReview/Ch17/Ch17_7
+# Build directory: /home/tseyu/cppReview/Ch17/Ch17_7/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ Ch17/Ch17_7/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iterator> // for std::size and std::ssize
+
+int main()
+{
+    const int prime[] { 2, 3, 5, 7, 11 };   // the compiler will deduce prime to have length 5
+
+    std::cout << std::size(prime) << '\n';  // C++17, returns unsigned integral value 5
+    std::cout << std::ssize(prime) << '\n'; // C++20, returns signed integral value 5
+    std::cout << sizeof(prime) / sizeof(prime[0]) << '\n';
+    return 0;
+}
