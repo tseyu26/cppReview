ae834e2b415dc5457029a2b8283e761c725ad4d5
tseyu <newbeeu2022@outlook.com>
2024-02-22 17:40:12 +0800

    add Ch17_1

---
+++ Ch17/Ch17_1/main.cpp
@@ -0,0 +1,10 @@
+#include <array>
+#include <iostream>
+
+int main()
+{
+    constexpr std::array a1 { 9, 7, 5, 3, 1 }; // The type is deduced to std::array<int, 5>
+    constexpr std::array a2 { 9.7, 7.31 };     // The type is deduced to std::array<double, 2>
+
+    return 0;
+}
