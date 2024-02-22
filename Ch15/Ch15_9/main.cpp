0035a6e4315c88713bccf77a89d985ca1b9e1267
tseyu <newbeeu2022@outlook.com>
2024-02-22 14:49:24 +0800

    add Ch15_9

---
+++ Ch15/Ch15_9/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+class Storage; // forward declaration for class Storage
+
+class Display
+{
+private:
+	bool m_displayIntFirst {};
+
+public:
+	Display(bool displayIntFirst)
+		: m_displayIntFirst { displayIntFirst }
+	{
+	}
+
+	void displayStorage(const Storage& storage); // forward declaration for Storage needed for reference here
+};
+
+class Storage // full definition of Storage class
+{
+private:
+	int m_nValue {};
+	double m_dValue {};
+public:
+	Storage(int nValue, double dValue)
+		: m_nValue { nValue }, m_dValue { dValue }
+	{
+	}
+
+	// Make the Display::displayStorage member function a friend of the Storage class
+	// Requires seeing the full definition of class Display (as displayStorage is a member)
+	friend void Display::displayStorage(const Storage& storage);
+};
+
+// Now we can define Display::displayStorage
+// Requires seeing the full definition of class Storage (as we access Storage members)
+void Display::displayStorage(const Storage& storage)
+{
+	if (m_displayIntFirst)
+		std::cout << storage.m_nValue << ' ' << storage.m_dValue << '\n';
+	else // display double first
+		std::cout << storage.m_dValue << ' ' << storage.m_nValue << '\n';
+}
+
+int main()
+{
+    Storage storage { 5, 6.7 };
+    Display display { false };
+    display.displayStorage(storage);
+
+    return 0;
+}
