1c16b0e02fae3ebea1aa121ee10638ec640868db
tseyu <newbeeu2022@outlook.com>
2024-03-01 15:29:43 +0800

    add Ch21_12

---
+++ Ch21/Ch21_12/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tseyu/cppReview/Ch21/Ch21_12
+# Build directory: /home/tseyu/cppReview/Ch21/Ch21_12/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
