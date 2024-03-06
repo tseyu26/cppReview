5bf5d53dbe394bc65b516f5964c9535399017573
tseyu <newbeeu2022@outlook.com>
2024-03-06 15:55:33 +0800

    add Ch22_5

---
+++ Ch22/Ch22_5/main.cpp
@@ -0,0 +1,35 @@
+#include <memory> // for std::unique_ptr
+#include <iostream>
+
+class Resource
+{
+public:
+	Resource() { std::cout << "Resource acquired\n"; }
+	~Resource() { std::cout << "Resource destroyed\n"; }
+
+	friend std::ostream& operator<<(std::ostream& out, const Resource &res)
+	{
+		out << "I am a resource";
+		return out;
+	}
+};
+
+// The function only uses the resource, so we'll accept a pointer to the resource, not a reference to the whole std::unique_ptr<Resource>
+void useResource(const Resource* res)
+{
+	if (res)
+		std::cout << *res << '\n';
+	else
+		std::cout << "No resource\n";
+}
+
+int main()
+{
+	auto ptr{ std::make_unique<Resource>() };
+
+	useResource(ptr.get()); // note: get() used here to get a pointer to the Resource
+
+	std::cout << "Ending program\n";
+
+	return 0;
+} // The Resource is destroyed here
