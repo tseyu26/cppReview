3bd071d7af4a0e2c276902ff33137bbf2f2b4864
tseyu <newbeeu2022@outlook.com>
2024-03-08 19:19:28 +0800

    add Ch24_2

---
+++ Ch24/Ch24_2/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <string_view>
+
+class Person
+{
+public:
+    std::string m_name{};
+    int m_age{};
+
+    Person(std::string_view name = "", int age = 0)
+        : m_name{name}, m_age{age}
+    {
+    }
+
+    const std::string& getName() const { return m_name; }
+    int getAge() const { return m_age; }
+
+};
+
+// Employee publicly inherits from Person
+class Employee: public Person
+{
+public:
+    double m_hourlySalary{};
+    long m_employeeID{};
+
+    Employee(double hourlySalary = 0.0, long employeeID = 0)
+        : m_hourlySalary{hourlySalary}, m_employeeID{employeeID}
+    {
+    }
+
+    void printNameAndSalary() const
+    {
+        std::cout << m_name << ": " << m_hourlySalary << '\n';
+    }
+};
+
+int main()
+{
+    Employee frank{20.25, 12345};
+    frank.m_name = "Frank"; // we can do this because m_name is public
+
+    frank.printNameAndSalary();
+
+    return 0;
+}
