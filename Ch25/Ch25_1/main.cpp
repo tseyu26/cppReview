e4d31554202d3b7474c6213eda4d37b5b6aa71b4
tseyu <newbeeu2022@outlook.com>
2024-03-11 15:28:07 +0800

    add Ch25_1

---
+++ Ch25/Ch25_1/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string_view>
+#include <string>
+
+class Animal
+{
+protected:
+    std::string m_name;
+
+    // We're making this constructor protected because
+    // we don't want people creating Animal objects directly,
+    // but we still want derived classes to be able to use it.
+    Animal(std::string_view name)
+        : m_name{ name }
+    {
+    }
+
+    // To prevent slicing (covered later)
+    Animal(const Animal&) = default;
+    Animal& operator=(const Animal&) = default;
+
+public:
+    std::string_view getName() const { return m_name; }
+    std::string_view speak() const { return "???"; }
+};
+
+class Cat: public Animal
+{
+public:
+    Cat(std::string_view name)
+        : Animal{ name }
+    {
+    }
+
+    std::string_view speak() const { return "Meow"; }
+};
+
+class Dog: public Animal
+{
+public:
+    Dog(std::string_view name)
+        : Animal{ name }
+    {
+    }
+
+    std::string_view speak() const { return "Woof"; }
+};
+
+int main()
+{
+    const Cat cat{ "Fred" };
+    std::cout << "cat is named " << cat.getName() << ", and it says " << cat.speak() << '\n';
+
+    const Dog dog{ "Garbo" };
+    std::cout << "dog is named " << dog.getName() << ", and it says " << dog.speak() << '\n';
+
+    const Animal* pAnimal{ &cat };
+    std::cout << "pAnimal is named " << pAnimal->getName() << ", and it says " << pAnimal->speak() << '\n';
+
+    pAnimal = &dog;
+    std::cout << "pAnimal is named " << pAnimal->getName() << ", and it says " << pAnimal->speak() << '\n';
+
+    return 0;
+}
