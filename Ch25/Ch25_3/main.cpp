b53e3ee6d0e594a30a0dfd9681400407906eb555
tseyu <newbeeu2022@outlook.com>
2024-03-11 16:12:07 +0800

    add Ch25_3

---
+++ Ch25/Ch25_3/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string_view>
+
+class A
+{
+public:
+	virtual std::string_view getName1(int x) { return "A"; }
+	virtual std::string_view getName2(int x) { return "A"; }
+};
+
+class B : public A
+{
+public:
+	virtual std::string_view getName1(int x) { return "B"; } // note: parameter is a short
+	virtual std::string_view getName2(int x) const { return "B"; } // note: function is const
+};
+
+int main()
+{
+	B b{};
+	A& rBase{ b };
+	std::cout << rBase.getName1(1) << '\n';
+	std::cout << rBase.getName2(2) << '\n';
+
+	return 0;
+}
