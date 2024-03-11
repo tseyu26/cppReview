ff82f51e76463dcf7fdbd7560616159d9e22c9ec
tseyu <newbeeu2022@outlook.com>
2024-03-11 15:50:08 +0800

    add Ch25_2

---
+++ Ch25/Ch25_2/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tseyu/cppReview/Ch25/Ch25_2
+# Build directory: /home/tseyu/cppReview/Ch25/Ch25_2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
