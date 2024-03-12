0ac2002ca3246a8ce3298de22edf4cfd652b3a91
tseyu <newbeeu2022@outlook.com>
2024-03-12 23:21:21 +0800

    add Ch25_7

---
+++ Ch25/Ch25_7/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <string_view>
+
+class Animal // This Animal is an abstract base class
+{
+protected:
+    std::string m_name {};
+
+public:
+    Animal(std::string_view name)
+        : m_name{ name }
+    {
+    }
+
+    const std::string& getName() const { return m_name; }
+    virtual std::string_view speak() const = 0; // note that speak is now a pure virtual function
+
+    virtual ~Animal() = default;
+};
+
+class Cow: public Animal
+{
+public:
+    Cow(std::string_view name)
+        : Animal(name)
+    {
+    }
+
+    std::string_view speak() const override { return "Moo"; }
+};
+
+int main()
+{
+    Cow cow{ "Betsy" };
+    std::cout << cow.getName() << " says " << cow.speak() << '\n';
+
+    return 0;
+}
