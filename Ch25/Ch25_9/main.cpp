544cf45f0bf8120f864ebb06c0e5903a25ecf2f4
tseyu <newbeeu2022@outlook.com>
2024-03-19 10:19:26 +0800

    add Ch25_9

---
+++ Ch25/Ch25_9/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string_view>
+
+class Base
+{
+protected:
+    int m_value{};
+
+public:
+    Base(int value)
+        : m_value{ value }
+    {
+    }
+
+    virtual ~Base() = default;
+
+    virtual std::string_view getName() const { return "Base"; }
+    int getValue() const { return m_value; }
+};
+
+class Derived: public Base
+{
+public:
+    Derived(int value)
+        : Base{ value }
+    {
+    }
+
+   std::string_view getName() const override { return "Derived"; }
+};
+
+int main()
+{
+    Derived derived{ 5 };
+    std::cout << "derived is a " << derived.getName() << " and has value " << derived.getValue() << '\n';
+
+    Base& ref{ derived };
+    std::cout << "ref is a " << ref.getName() << " and has value " << ref.getValue() << '\n';
+
+    Base* ptr{ &derived };
+    std::cout << "ptr is a " << ptr->getName() << " and has value " << ptr->getValue() << '\n';
+
+    return 0;
+}
